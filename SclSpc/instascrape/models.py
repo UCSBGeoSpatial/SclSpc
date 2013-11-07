ac5a3ca1609b24e64c3b9d2a9decb7334c54a48f
Marcus Bernales <mboperator@gmail.com>
2013-11-07 11:40:55 -0800

    Instagram cron scrape implemented

--- SclSpc/instascrape/models.py
+++ SclSpc/instascrape/models.py
@@ -125,16 +125,17 @@ class InstagramInterface(models.Model):
     return
 
 class overviewScrape(CronJobBase):
-  RUN_EVERY_MINS = 60 #every hour
+  RUN_EVERY_MINS = 120 #every hour
   schedule = Schedule(run_every_mins = RUN_EVERY_MINS)
   code = 'instascrape.overviewScrape'
   def do(self):
     inst = InstagramInterface.objects.all()[0]
-    photos = inst.overview_scrape()
-    try:
-      inst.save_pics(photos)
-    except:
-      print "An error has occured in saving pics"
+    for i in range(0,4999):
+      photos = inst.overview_scrape()
+      try:
+        inst.save_pics(photos)
+      except:
+        print "An error has occured in saving pics"
 
   CRON_CLASSES = [
     "instascrape.cron.overviewScrape"
