e6a2c63ecdec2638880c5024b51f87d2834953aa
Marcus Bernales <mboperator@gmail.com>
2013-11-20 16:41:53 -0800

    Edited admin.py, removed filterable lists, didn't work as expected.

--- SclSpc/dataman/admin.py
+++ SclSpc/dataman/admin.py
@@ -5,13 +5,17 @@ class PicAdmin(admin.ModelAdmin):
   
   list_display = ('name', 'place', 'category', 'created')
   ordering = ('-created_at',)
-  list_filter = ('location__place', 'location__place__foursq_primary_cat')
+
+class PlaceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'foursq_primary_cat')
+  ordering = ('foursq_primary_cat',)
+  list_filter = ('foursq_primary_cat',)
 
 #Defines which models are accessible via Admin interface
 #gis.admin allows java slippy map :D
 admin.site.register(Location, admin.GeoModelAdmin)
 admin.site.register(Pic, PicAdmin)
 admin.site.register(CheckIn, admin.GeoModelAdmin)
-admin.site.register(Place, admin.GeoModelAdmin)
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Tag, admin.GeoModelAdmin)
 admin.site.register(Category, admin.GeoModelAdmin)
